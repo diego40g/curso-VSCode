12479d5748e3f7e600cd79dd527c058db17f8977
diego40g <diegopaznaula@hotmail.com>
2022-10-26 23:45:41 -0500

    space with multiple cursor

--- src/app/02-multi-cursor-edicion/04-multi-cursor-formato.ts
+++ src/app/02-multi-cursor-edicion/04-multi-cursor-formato.ts
@@ -14,6 +14,11 @@ const ironman = 'tony stark';
 const spiderman = 'peter parker';
 const viudaNegra = 'natalia romanova';
 
+const hulk       = 'brouce banner';
+const Hawkeye    = 'cinton francis';
+const ironman    = 'tony stark';
+const spiderman  = 'peter parker';
+const viudaNegra = 'natalia romanova';
 
 // Objetivo final (sin los comentarios)
 // const hulk       = 'brouce banner';
